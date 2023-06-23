0a670c0d9b65fd0a370edf122fba929dd87066f1
Oleksandr Ryzhykov <ryzhykov.oleksandr@gmail.com>
2023-06-23 23:07:17 +0700

    Add store prvider in App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Provider } from "react-redux";
+import { configureStore } from "@reduxjs/toolkit";
+import usersReducer from "./features/usersSlice";
+import UserList from "./components/UserList";
 
-function App() {
+const store = configureStore({
+  reducer: {
+    users: usersReducer,
+  },
+});
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <UserList />
+    </Provider>
   );
-}
+};
 
 export default App;
